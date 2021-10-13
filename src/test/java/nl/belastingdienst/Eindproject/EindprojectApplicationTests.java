6336a237a7966cb0902082ba33b1f4cc07dd980e
Jelmer <jelmerdijxhoorn@hotmail.com>
2021-10-13 14:18:36 +0200

    Opzet Eindproject

---
+++ src/test/java/nl/belastingdienst/Eindproject/EindprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.belastingdienst.Eindproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EindprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
