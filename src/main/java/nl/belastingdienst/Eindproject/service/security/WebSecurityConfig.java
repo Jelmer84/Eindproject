f71643ee9f109091fb2e025fc47b6d286bddc08b
Jelmer84 <74977931+Jelmer84@users.noreply.github.com>
2021-11-18 22:31:16 +0100

    WebSecurityConfig

--- src/main/java/nl/belastingdienst/Eindproject/service/security/WebSecurityConfig.java
+++ src/main/java/nl/belastingdienst/Eindproject/service/security/WebSecurityConfig.java
@@ -20,7 +20,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 /**
  * Hier gebruiken we de EnableGlobalMethodSecurity(prePostIsEnabled = true) om de @PreAuthorize annotaties te gebruiken
- * op andere plekken in de applicatie.
+ * op andere plekken in de applicatie
  */
 @Configuration
 @EnableWebSecurity
