44e504cc312e5f6bd7315293c72f918faa512f3b
Jelmer84 <74977931+Jelmer84@users.noreply.github.com>
2021-11-18 22:17:48 +0100

    Controllers opzet & WebSecurityConfig

---
+++ src/main/java/nl/belastingdienst/Eindproject/service/BackOfficeService.java
@@ -0,0 +1,12 @@
+package nl.belastingdienst.Eindproject.service;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BackOfficeService {
+
+    @PreAuthorize("hasRole('BACKOFFICE')")
+    public String generateBackContent() {return "Back Office Board.";}
+
+}
